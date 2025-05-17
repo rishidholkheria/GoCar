5d66aa3fb19b5bb284d0c88af8d26cc0b7f95358
Rishi Dholkheria <rishidholkheria2001@gmail.com>
2025-05-18 03:41:22 +0530

    Hero Section

---
+++ src/app/page.tsx
@@ -0,0 +1,22 @@
+import HeroSection from "./pages/HeroSection";
+
+export default function Home() {
+  return (
+    <div className="w-full h-screen">
+      <HeroSection/>
+      <h2>aasdjsadjk</h2>
+      <h2>aasdjsadjk</h2>
+      <h2>aasdjsadjk</h2>
+      <h2>aasdjsadjk</h2>
+      <h2>aasdjsadjk</h2>
+      <h2>aasdjsadjk</h2>
+      <h2>aasdjsadjk</h2>
+      <h2>aasdjsadjk</h2>
+      <h2>aasdjsadjk</h2>
+      <h2>aasdjsadjk</h2>
+      <h2>aasdjsadjk</h2>
+      <h2>aasdjsadjk</h2>
+      <h2>aasdjsadjk</h2>
+    </div>
+  );
+}
---
+++ src/app/pages/HeroSection.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import ResizableNavbar from "../components/ResizableNavbar";
+
+const HeroSection = () => {
+  return (
+    <div className="w-full h-screen px-4 flex flex-col items-center">
+      <ResizableNavbar />
+      <video
+        autoPlay
+        muted
+        loop
+        playsInline
+        className="text-black backdrop-opacity-75 relative w-full h-4/5 rounded-2xl object-cover z-[-1]"
+      >
+        <source src="/assets/heroVideo.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div className="mx-auto absolute bottom-0 bg-white w-3/4 min-h-[100px] flex rounded-2xl shadow-[#D9D9D9]">
+            <div className="px-4 my-auto border-r-1 border-gray-500 w-1/3">
+                <p className="text-gray-900 font-bold text-sm">From</p>
+                <input type="text" placeholder="Pickup Point" className="text-2xl outline-none py-2 capitalize"/>
+            </div>
+            <div className="px-4 my-auto border-r-1 border-gray-500 w-1/3">
+                <p className="text-gray-900 font-bold text-sm">To</p>
+                <input type="text" placeholder="Select Drop" className="text-2xl outline-none py-2 capitalize"/>
+            </div>
+            <div className="px-4 my-auto w-1/3">
+                <p className="text-gray-900 font-bold text-sm">Pick-Up Date & Time</p>
+                <input type="text" placeholder="18 May 2025, 1:00 PM" className="text-2xl outline-none py-2 capitalize"/>
+            </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
